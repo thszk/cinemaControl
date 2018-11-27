e606663c36f6d93852e65098b15fd459f5a10381
Maqdo <45201107+Maqdo@users.noreply.github.com>
2018-11-27 00:11:09 -0200

    Inserindo dados novos no Script
    Inseridos dados extras no Script

--- script_criacao.sql
+++ script_criacao.sql
@@ -120,15 +120,24 @@ update sessao set dia = '2018-11-01' where cod_sessao = 1;
 
 /*inserindo dados da sessao*/
 insert into sessao (cod_sessao, hora, qt_ingressos, cod_sala, cod_filme) values (01, '10:30', 50, 1, 1);
+insert into sessao (cod_sessao, hora, qt_ingressos, cod_sala, cod_filme) values (02, '15:30', 100, 2, 2);
+insert into sessao (cod_sessao, hora, qt_ingressos, cod_sala, cod_filme) values (03, '20:00', 150, 6, 4);
+insert into sessao (cod_sessao, hora, qt_ingressos, cod_sala, cod_filme) values (04, '23:00', 120, 4, 3);
 
 /*dados do tipo de ingresso*/
 insert into tipo_ingresso (descricao, preco) values ('inteira',20.00),('meia',10.00),('cortesia',0.00);
 
 /*dados da compra de ingressos*/
 insert into ingresso (cod_sessao,cod_filme, cod_sala, cod_tipo_ingresso) values (01,1,1,1);
+insert into ingresso (cod_sessao,cod_filme, cod_sala, cod_tipo_ingresso) values (02,2,2,1);
+insert into ingresso (cod_sessao,cod_filme, cod_sala, cod_tipo_ingresso) values (03,4,6,2);
+insert into ingresso (cod_sessao,cod_filme, cod_sala, cod_tipo_ingresso) values (04,3,4,3);
 
 /*quando compra ingresso atualiza a tabela de sessao decrementando os ingressos disponiveis*/
 update sessao set qt_ingressos = qt_ingressos - 1 where cod_sessao = 1 ;
+update sessao set qt_ingressos = qt_ingressos - 1 where cod_sessao = 2 ;
+update sessao set qt_ingressos = qt_ingressos - 1 where cod_sessao = 3 ;
+update sessao set qt_ingressos = qt_ingressos - 1 where cod_sessao = 4 ;
 
 /*exibicao de dados do ingresso para o usuario - usando elementos apresentado em sala -  exibe: nome do filme, genero, sala, data, hora da sessao, tipo*/
 select ingresso.cod_ingresso as codigo_ingresso, filme.titulo as filme, genero.descricao as genero, sessao.cod_sala as sala, sessao.dia as data, sessao.hora as hora, tipo_ingresso.descricao from ingresso
